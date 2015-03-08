fb74ea693f64a07d5b974bf933c56d95c4a6e405
Ian Gibson <Ian.Gibson222@gmail.com>
2015-03-08 17:39:45 -0400

    First commit

---
+++ bf_int.py
@@ -0,0 +1,63 @@
+# Author: Ian Gibson
+# Last Change: 3/8/2015
+# A basic Brainf*ck interpreter
+# No optimization, just implementing control characters
+# Tested using Python 2.7.6
+
+import sys
+
+#read file into string
+infile = open(sys.argv[1], 'r')
+program = infile.read()
+infile.close()
+
+#initialize variables
+stack = []
+data = [0]
+dptr = 0
+cptr = 0
+bracketDepth = 0
+skip = False
+
+#execute program
+while cptr < len(program):
+	ch = program[cptr]
+	if skip == False:
+		if ch == '+':
+			data[dptr] += 1
+	        if ch == '-':
+			data[dptr] -= 1
+		if ch == '>':
+			dptr += 1
+                	if len(data) <= dptr:	#extend data list if needed
+				data.append(0)
+		if ch == '<':
+			dptr -= 1
+		if ch == '.':
+			sys.stdout.write(chr(data[dptr]))
+		if ch == ',':
+			data[dptr] = ord(sys.stdin.read(1))
+		if ch == '[':
+			#if the current data value is not 0, remember the location
+			#otherwise skip this block
+			if data[dptr]:
+				stack.append(cptr)
+			else:
+				skip = True			
+		if ch == ']':
+			#if the current data value is not 0, go back to the matching opening bracket
+			#otherwise remove the bracket pair from the stack and continue
+			if data[dptr]:
+				cptr = stack[len(stack)-1]
+			else:
+				stack.pop()
+
+	#skipping loop, check bracket levels
+	else:
+		if ch == '[':
+			bracketDepth += 1
+		elif ch == ']' and bracketDepth:
+			bracketDepth -= 1
+		elif ch == ']':
+			skip = False
+	cptr += 1
